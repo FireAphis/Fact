d5941b5fe1308b8d2b52acdb003b3ec270d14d7a
FireAphis <FireAphis@gmail.com>
2011-05-19 20:16:51 +0300

    The initial commit. Currently the existing code supports a CLI based flow that shows all the not delivered activities in the current stream. After an activity is selected, its change set is displayed.

---
+++ activities_cli.rb
@@ -0,0 +1,84 @@
+
+require "rubygems"
+require "highline/import"
+require "~/dev/fact/clearcase"
+
+module Fact
+
+class ActivitiesCli
+
+  #
+  #
+  def ActivitiesCli.choose_undelivered_activity
+
+    stream = ClearCase.get_current_stream
+    return if stream==""
+
+    puts ""
+    say("The current stream is <%= color('#{stream}', BOLD) %>. Fetching the stream activities... ")
+    activities = ClearCase.get_activities
+    say("Done.")
+
+    if activities.empty?
+      say("No undelivered activities.")
+    else
+      say("These are the undelivered activities in the current stream:")
+
+      return choose do |menu|
+        menu.prompt    = "Enter the activity number: "
+        menu.select_by = :index
+
+        # Add a menu entry for each activity
+        activities.each do |activity|
+          menu.choice(activity[:headline]) { activity[:name] }
+        end
+
+      end
+    end
+
+  end
+
+
+  #
+  #
+  def ActivitiesCli.choose_file_from_activity(activity_name)
+
+    puts ""
+    say("Fetching the change set for the activity <%= color('#{activity_name}', BOLD) %>... ")
+    changeset = ClearCase.get_activity_change_set(activity_name)
+    say("Done.")
+
+    if changeset.empty?
+      say("The changeset is empty.")
+    else
+      say("The activity contains the following files:")
+
+      # The method will return the choice from the menu
+      return choose do |menu|
+        menu.prompt    = "Enter the file number: "
+        menu.select_by = :index
+
+        #  Add a menu entry for each file in the changeset
+        changeset.each do |file, versions|
+          # Suffix contains the versions count and the check-out indicator
+          suffix = "version#{"s" unless versions.size<2}) #{"<%= color('CHECKED-OUT!', :red) %>" if versions.last=~/CHECKEDOUT/}"
+          menu.choice("#{file} (#{versions.size} #{suffix}") { file }
+        end
+
+      end
+    end
+
+  end
+
+
+end
+
+end
+
+
+
+activity = Fact::ActivitiesCli.choose_undelivered_activity
+unless activity.nil?
+  file = Fact::ActivitiesCli.choose_file_from_activity(activity)
+  puts Fact::ClearCase.get_file_info(file)
+end
---
+++ clearcase.rb
@@ -0,0 +1,120 @@
+
+
+module Fact
+
+class ClearCase
+
+  # Get the name of the current stream.
+  #
+  def ClearCase.get_current_stream
+    return `cleartool lsstream -s`.rstrip
+  end
+
+  #
+  #
+  def ClearCase.get_previous_version(file, version)
+    return `cleartool desc -pred -s #{file}@@#{version}`.rstrip
+  end
+
+  # Get all the non-obsolete activities in the current view.
+  # Returns an array of hashes. Each hash represents an activity and contains 
+  # two keys: :name and :headline.
+  #
+  def ClearCase.get_activities
+    return parse_lsact_output(`cleartool lsact -fmt "%[name]p,%[headline]p,"`)
+  end
+
+  #
+  #
+  def ClearCase.get_file_info(file)
+    # Get the properties of the latest version of the file
+    curr_version_info = parse_describe_file(`cleartool desc -fmt "version=%Sn, activity=%[activity]p, date=%Sd, type=%m, predecessor=%PVn" #{file}`)
+    return if curr_version_info.nil?
+
+    # Retreive all the change set to find the earliest version of the file in the change set
+    change_set = get_activity_change_set(curr_version_info[:activity])
+    return if change_set.nil?
+
+    versions = change_set[file]
+
+    changeset_predecessor = get_previous_version(file, versions.first)
+
+    return [curr_version_info.to_a, versions, changeset_predecessor]
+  end
+
+  # Returns the changes that were made as a part of the specified activity.
+  # Returns a hash where the key is a file names and the value is a list of versions of 
+  # that file. If there are no files in the activity, returns an empty hash.
+  #
+  def ClearCase.get_activity_change_set(activity_name)
+
+    # Get a string that is space separated list of all the versions in the activity
+    versions_dump = `cleartool lsact -fmt "%[versions]p" #{activity_name}`
+
+    # Will convert the string into a hash of filenames and lists of version numbers
+    change_set = Hash.new { |hash,key| hash[key]=[] }
+
+    # Convert the string into a sorted array and iterate it
+    versions_dump.split(" ").sort.each do |version_str|
+      version = parse_cc_version(version_str)
+      change_set[version[:file]].push("#{version[:branch]}/#{version[:version_number]}")
+    end
+
+    return change_set
+  end
+
+  # Launches the default diff tool comparing the specified versions.
+  # The parameters are supposed to be strings that are qualified parameters for
+  # ClearCase diff command.
+  #
+  def ClearCase.diff_graphical(version1, version2)
+    diff_process = fork { exec "cleartool diff -gra #{version1} #{version2}" }
+    Process.detach(diff_process)
+  end
+
+  # Parses a string representing ClearCase element version and converts it into a hash
+  # with a key for each part of the version string.
+  #
+  # version_str is expected to be of the following form:
+  #
+  #       <file full path>@@<branch name>/{<version number>|CHECKEDOUT.<checkout number>}
+  #
+  # Returns a hash with the following keys: :file, :branch, :version_number.
+  #
+  def ClearCase.parse_cc_version(version_str)
+    if version_str =~ /(.*)@@(.+)\/(CHECKEDOUT\.)?(\d+)$/
+      return { :file => $1, :branch => $2, :version_number => "#{$3}#{$4}" }
+    end
+  end
+
+  #
+  #
+  def ClearCase.parse_describe_file(describe_str)
+    if describe_str =~ /version=(.*), activity=(.*), date=(.*), type=(.*), predecessor=(.*)$/
+      return { :version=>$1, :activity=>$2, :date=>$3, :type=>$4, :predecessor=>$5 }
+    end
+  end
+
+  # Converts the textual result of the lsact command into an array of activities.
+  # The input string is expected to be of the following format:
+  #
+  #       (<activity name>,<activity headline>,)*
+  #
+  # Returns an array of hashes. Each hash has two keys: :name and :headline.
+  #
+  def ClearCase.parse_lsact_output(lsact)
+
+    activities = []
+
+    # Takes the string and convers it to an array of name-headline pairs
+    lsact.scan(/(.*?),(.*?),/).each do |pair|
+      activities += [{ :name => pair[0], :headline => pair[1] }]
+    end
+
+    return activities
+  end
+
+end
+
+end
+
